9251a14d1226051506d2c2a5289b7354e256f432
Chris Rickard <103567821+rickardc@users.noreply.github.com>
2023-07-06 10:06:59 +1000

    added employee search

--- lab4/code/employee_search.php
+++ lab4/code/employee_search.php
@@ -18,14 +18,14 @@
         action="employee_search.php"
         method="get">
 
-        <label for="make">Select make:</label>
-        <select name='make' id='make'>
-            <option value=''>Select Make</option>
+        <label for="city">Please select which city: </label>
+        <select name='city' id='city'>
+            <option value=''>Select City</option>
             <option value='all'>All</option>
             <?php
 
                 // sql query
-                $SQLstring = "select distinct make from inventory";
+                $SQLstring = "select distinct city from employees";
 
                 // query result
                 $queryResult = @mysqli_query($DBConnect, $SQLstring)
@@ -39,32 +39,74 @@
                 ?>
         </select>
         <br>
+        <label for="lang">Please select which language: </label>
+        <select name='lang' id='lang'>
+            <option value=''>Select Language</option>
+            <option value='all'>All</option>
+            <?php
+
+                // sql query
+                $SQLstring = "select distinct language from Languages";
+
+                // query result
+                $queryResult = @mysqli_query($DBConnect, $SQLstring)
+                    Or die ("<p>Unable to query the $TableName table.</p>"."<p>Error code ". mysqli_errno($DBConnect). ": ".mysqli_error($DBConnect)). "</p>";
+
+                // loop through query result
+                while ($row = mysqli_fetch_row($queryResult)) {
+                    echo "<option value='{$row[0]}'>{$row[0]}</option>";
+                }
+
+            ?>
+        </select>
+
+        <br>
+        <label for="years">Please select how many years experience: </label>
+        <input type="number" name="years" id="years" min="0" max="50">
+
+        <br>
+        <br>
         <input type="submit" value="Search">
         <br>
     </form>
 
     <h2>Results</h2>
-    <?php
-         
-    if(isset($_GET['make'])) {
-
-        $make = $_GET['make'];
+    
+    <?php     
+    if(isset($_GET['city']) && isset($_GET['lang']) && isset($_GET['years'])) {
+        $city = $_GET['city'];
+        $lang = $_GET['lang'];
+        $years = $_GET['years'];
         
-        if ($make == 'all') {
-            $SQLstring = "select * from inventory order by price desc";
-        } else {
-            $SQLstring = "select * from inventory where make = '$make' order by price desc";
+        $SQLstring = "SELECT Employees.first_name, Employees.last_name, Languages.language, Experience.years, Employees.city
+        FROM Employees
+        JOIN Experience ON Experience.employee_id = Employees.id
+        JOIN Languages ON lang_id = Experience.language_id";
+
+        if($city != 'all') {
+            $SQLstring .= " and city = '$city'";
+        }
+        
+        if($lang != 'all') {
+            $SQLstring .= " and language = '$lang'";
+        }
+        
+        if($years != '') {
+            $SQLstring .= " and Experience.years >= $years";
         }
         
         $queryResult = @mysqli_query($DBConnect, $SQLstring)
             Or die ("<p>Unable to query the $TableName table.</p>"."<p>Error code ". mysqli_errno($DBConnect). ": ".mysqli_error($DBConnect)). "</p>";
         
-            echo "<table border='1'>";
-        echo "<tr><th>Make</th><th>Model</th><th>Price</th><th>Quantity</th></tr>";
+        echo $SQLstring;
+        echo "\n <table border='1'>";
+        echo "\n <tr><th>First Name</th><th>Last Name</th><th>Language</th><th>Years EXP</th><th>City</th></tr>";
+        
         while ($row = mysqli_fetch_row($queryResult)) {
-            echo "<tr><td>{$row[1]}</td><td>{$row[2]}</td><td>{$row[3]}</td><td>{$row[4]}</td></tr>";
+            echo "\n <tr><td>{$row[0]}</td><td>{$row[1]}</td><td>{$row[2]}</td><td>{$row[3]}</td><td>{$row[4]}</td></tr>";
         }
-        echo "</table>";
+        
+        echo "\n </table>";
     }
             
 
